a573029ced12e411124c1dd40ef6a2b52700f3a1
maleksandrowicz93 <mateusz.aleksandrowicz93@gmail.com>
2023-02-17 11:28:30 +0100

    generate edit-page component

---
+++ src/app/edit-form-page/edit-form-page.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-edit-form-page',
+  templateUrl: './edit-form-page.component.html',
+  styleUrls: ['./edit-form-page.component.css']
+})
+export class EditFormPageComponent {
+
+}
