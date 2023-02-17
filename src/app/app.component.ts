f86f99466fe82b99340cf1681c729f4a04681882
maleksandrowicz93 <mateusz.aleksandrowicz93@gmail.com>
2023-02-17 11:29:37 +0100

    partial implementation of app-root prototype

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Page } from './enums/page';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,17 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'cqrs-demo-fe';
+
+  currentPage = Page.MAIN_PAGE;
+  page = Page;
+  studentId = "";
+
+  showDetails(studentId: string): void {
+    this.studentId = studentId;
+    this.currentPage = Page.USER_PAGE;
+  }
+
+  showMainPage(): void {
+    this.currentPage = Page.MAIN_PAGE;
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MainPageComponent } from './main-page/main-page.component';
+import { EditFormPageComponent } from './edit-form-page/edit-form-page.component';
+import { StudentEntryComponent } from './student-entry/student-entry.component';
+import { StudentPageComponent } from './student-page/student-page.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MainPageComponent,
+    EditFormPageComponent,
+    StudentEntryComponent,
+    StudentPageComponent
   ],
   imports: [
     BrowserModule,
